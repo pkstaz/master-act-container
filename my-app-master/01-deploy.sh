d5147bf4b4779247f39c1e44f0097d727545553d
Carlos Estay <cestay@redhat.com>
2022-04-14 14:45:33 -0400

    Actividad final contenedores

--- my-app-master/01-deploy.sh
+++ my-app-master/01-deploy.sh
@@ -6,7 +6,7 @@
 # Usuario: estaygonzalez
 # Contraseña: OBSestaygonzalez2022
 #
-# Para la solución de este problema se crean variables en este archo en la cual se pide reemplazar por lo menos:
+# Para la solución de este problema se crean variables en este archivo en la cual se pide reemplazar por lo menos:
 # REGISTRY_USER --> Usuario con el que se inicia sesion
 # REGISTRY_SERVER --> Registry endpoint. Ejemplo docker.io o https://harbor.tallerdevops.com/
 #
