d0d2012f54ff514bbacab80409ee9145d4db2cad
Carlos Estay <cestay@redhat.com>
2022-04-14 15:10:35 -0400

    update clean script

--- my-app-master/02-clean.sh
+++ my-app-master/02-clean.sh
@@ -2,4 +2,11 @@
 
 docker container stop my-postgres-master && docker container rm my-postgres-master
 docker container stop my-app-master && docker container rm my-app-master
-docker network rm my-net-master
+docker network rm my-net-master 
+docker network rm my-app-master_default
+docker network rm my-app-master_my-net-master
+docker volume rm my-app-master_my-app-master-volume
+docker volume rm my-app-master_my-postgres-master-volume
+
+docker image rm my-app-master_quarkus
+docker image rm postgres
